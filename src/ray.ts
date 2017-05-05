00eaeba886fb108efb2aab8127f0c996c2a22fc1
New123User321 <Ny123Mail321@gmail.com>
2017-05-05 16:38:30 +0200

    Update ray.ts

--- src/ray.ts
+++ src/ray.ts
@@ -14,19 +14,20 @@ class Sphere
     {
         var A = camara.direction.x * camara.direction.x + camara.direction.y * camara.direction.y + camara.direction.z * camara.direction.z;
         var B = (camara.direction.x * (camara.pos.x - this.pos.x))
-                + (camara.direction.y * (camara.pos.y - this.pos.y))
-                + (camara.direction.z * (camara.pos.z - this.pos.z));
-        var C =  ((camara.pos.x - this.pos.x) * (camara.pos.x - this.pos.x)
-                + (camara.pos.y - this.pos.y) * (camara.pos.y - this.pos.y)
-                + (camara.pos.z - this.pos.z) * (camara.pos.z - this.pos.z)) - (this.radius * this.radius);
+            + (camara.direction.y * (camara.pos.y - this.pos.y))
+            + (camara.direction.z * (camara.pos.z - this.pos.z));
+        var C = ((camara.pos.x - this.pos.x) * (camara.pos.x - this.pos.x)
+            + (camara.pos.y - this.pos.y) * (camara.pos.y - this.pos.y)
+            + (camara.pos.z - this.pos.z) * (camara.pos.z - this.pos.z)) - (this.radius * this.radius);
 
         var D = (B * B) - (A * C);
 
-        if(D > 0)
+        if (D > 0)
         {
             var t;
             var t1 = (-B + Math.sqrt(D)) / A;
             var t2 = (-B - Math.sqrt(D)) / A;
+
             if (t1 < t2)
             {
                 t = t1;
@@ -35,9 +36,9 @@ class Sphere
             {
                 t = t2;
             }
-            return(new Vector(camara.pos.x + camara.direction.x * t ,camara.pos.y + camara.direction.y * t , camara.pos.z + camara.direction.z * t));
+            return (new Vector(camara.pos.x + camara.direction.x * t, camara.pos.y + camara.direction.y * t, camara.pos.z + camara.direction.z * t));
         }
-        else{return(null)}
+        else { return (null) }
     }
     colorat()
     {
@@ -58,13 +59,33 @@ class Vector
         this.y = y;
         this.z = z;
     }
+
     distance(Vector)
     {
-        dX = this.x - Vector.x;
-        dY = this.y - Vector.y;
-        dZ = this.z - Vector.z;
+        var dX = this.x - Vector.x;
+        var dY = this.y - Vector.y;
+        var dZ = this.z - Vector.z;
 
-        return(Math.sqrt((dX * dX) + (dY * dY) + (dZ * dZ)))
+        return (Math.sqrt((dX * dX) + (dY * dY) + (dZ * dZ)))
+    }
+
+    pount(vector: Vector)
+    {
+        return (new Vector(this.x * vector.x, this.y * vector.y, this.z * vector.z))
+    }
+    plus(vektor: Vector)
+    {
+        var newVector = new Vector(this.x + vektor.x, this.y + vektor.y, this.z + vektor.z);
+        return (newVector)
+    }
+    minus(vector: Vector)
+    {
+        var newVector = new Vector(this.x - vector.x, this.y - vector.y, this.z - vector.z)
+        return (newVector)
+    }
+    sum()
+    {
+        return (this.x + this.y + this.z)
     }
 }
 
