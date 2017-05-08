a11055a2f91d5d8d59628cf85e990e2600114501
Søren Magnusson <smag@tec.dk>
2017-05-08 16:17:58 +0200

    unitTest sat op. Kør jazzmine.bat og kør jasmine-ts

---
+++ jazzmine.bat
@@ -0,0 +1,9 @@
+REM /// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
+REM     let Jasmine = require('jasmine');
+
+
+npm install jasmine-core jasmine @types/jasmine jasmine-ts --save-dev
+rem npm install -g jasmine jasmine-ts
+npm install @types/node --save-dev
+
+rem ./node_modules/.bin/jasmine-ts src/something.spec.ts
