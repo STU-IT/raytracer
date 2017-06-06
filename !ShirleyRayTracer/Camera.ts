bd327fb3f8ec605cd15e36c9889f1a2542fdd2e4
new123user321 <new>
2017-06-06 12:15:45 +0200

    Shirly Raytracer kan lave kugler og trekanter og vise dem på HTML canvas

--- !ShirleyRayTracer/Camera.ts
+++ !ShirleyRayTracer/Camera.ts
@@ -29,3 +29,89 @@ class Camera
 
     }
 }
+
+//C++
+/*
+ II Camera.h
+ #ifndef CAMERA H
+ #define CAMERA_H_
+ #include "Ray.h"
+ #include "ONB.h"
+ class Camera
+ {
+ public:
+ Camera() {}
+ Camera(const Camera& orig)
+ {
+
+ center
+ corner
+ across
+ up
+ uvw
+ uO
+ u1
+ vO
+ v1
+ d
+ orig.center;
+ orig.corner;
+ orig.across;
+ orig.up;
+ orig.uvw;
+ orig.uO;
+ orig.u1;
+ orig.vO;
+ orig.vi;
+ orig.d;
+ lens radius = orig.lens_radius;
+ }
+ center
+ corner
+ across
+ up
+ uvw
+ uO
+ u1
+ vO
+ v1
+ d
+ orig.center;
+ orig.corner;
+ orig.across;
+ orig.up;
+ orig.uvw;
+ orig.uO;
+ orig.u1;
+ orig.vO;
+ orig.vi;
+ orig.d;
+ lens radius = orig.lens_radius;
+ Camera(Vector3 c, Vector3 gaze, Vector3 vup, float aperture, float left,
+ float right, float bottom, float top, float di stance )
+ : center(c), d(distance ), uO(left), u1(right), vO(bottom), v1(top)
+ {
+ lens_radius = aperturel2.0F;
+ uvw.initFromWV( -gaze, vup );
+ corner = center + uO*uvw.u() + vO*uvw.v() - d*uvw.w();
+ across = (u0-u1)*uvw.u();
+ up = (v0-v1)*uvw.v();
+ }
+
+ II a and b are the pixel positions
+ II xii and xi2 are the lens samples in the range (0, 1)􀀹2
+ Ray getRay(float a, float b, float xii, float xi2)
+ {
+ Vector3 origin = center + 2.0F*(xi1-0.5F) *lens_radius*uvw.u() +
+ 2.0F*(xi2-0.5F) *lens_radius*uvw.v();
+ };
+ }
+ Vector3 target = corner + across*a + up*b;
+ return Ray(origin, unitVector(target-origin));
+ Vector3 center, corner, across, up;
+ ONB uvw;
+ float lens_radius;
+ float uO, u1, vO, v1;
+ float d;
+ #endif II CAMERA H
+ */
